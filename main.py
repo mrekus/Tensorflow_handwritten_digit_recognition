5635dc9afefc7d37d1d1f2170eafacac048f545c
Marius R <marius.rekus@gmail.com>
2022-11-30 12:12:27 +0200

    Padalintas dataset, normalizuoti duomenys

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
 import cv2
 
+# Užkraunamas mnist dataset iš tensorflow
 mnist = tf.keras.datasets.mnist
+
+# Padalinami duomenys į train ir test datasets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# Normalizuojam duomenis, iš (0 - 255 juodų, baltų)(dalinama iš 255)
+X_train = tf.keras.utils.normalize(X_train, axis=1)
+X_test = tf.keras.utils.normalize(X_test, axis=1)
 
